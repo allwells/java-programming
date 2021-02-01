08bf78dd6eb29051ba4901965f9c64ce43eadf5e
allwells <aleenfestus@gmail.com>
2021-02-01 17:03:47 +0100

    Variables

---
+++ Variables/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "yourname";
+        int age = 12;
+        double weight = 19.99;
+        char letterA = 'A';
+        boolean choice = true;
+
+        System.out.println("String: " + name);
+        System.out.println("int: " + age);
+        System.out.println("double: " + weight);
+        System.out.println("char: " + letterA);
+        System.out.println("boolean: " + choice);
+
+        System.out.println("\n======================================================");
+        name = "Allwell"; 
+        /** name is now 'Allwell' and NOT 'yourname'
+         * This also applies for all data types
+        */
+        System.out.println(name);
+        
+    }
+}
