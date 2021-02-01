fa5e2e506ce0dc492146d2a658079ebaa67362b5
allwells <aleenfestus@gmail.com>
2021-02-01 16:39:05 +0100

    Hello World

---
+++ Comments/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+        /**
+         * Comments come in two forms in java
+         * the sigle line comment as shown below and the multiline comment as this one
+         * in which this text is in
+         */
+
+        // System.out.println("This has been commented and will not printout");
+        System.out.println("This will be printed");
+    }
+}
