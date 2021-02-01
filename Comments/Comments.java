59d9e60c1f587b39ce5777f17820291921c034a1
allwells <aleenfestus@gmail.com>
2021-02-01 16:41:11 +0100

    Comments

--- Comments/Comments.java
+++ Comments/Comments.java
@@ -1,9 +1,9 @@
 public class Comments {
     public static void main(String[] args) {
         /**
-         * Comments come in two forms in java
-         * the sigle line comment as shown below and the multiline comment as this one
-         * in which this text is in
+         * There are two types of comments in Java
+         * The Single Line Comment as shown below and 
+         * The Multi-Line Comment as this one
          */
 
         // System.out.println("This has been commented and will not printout");
