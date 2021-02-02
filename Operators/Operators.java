55e797caac7b23c6387d87a8b90ed7b40b2581f3
allwells <aleenfestus@gmail.com>
2021-02-02 02:03:41 +0100

    Add Operators

---
+++ Operators/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+    public static void main(String[] args) {
+        int sum1  = 100 + 50; // 150 (100 + 50)
+        int sum2 = sum1 + 250; // 400 (150 + 250)
+        int sum3 = sum2 + sum2; // 800 (400 + 400)
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+        /**
+         * Java divides the operators into the following groups:
+         *  - Arithmetic Operators
+         *  - Assignment Operators
+         *  - Comparison Operators
+         *  - Logical Operators
+         *  - Bitwise Operators
+         */
+
+    }
+}
