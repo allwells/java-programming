87321b3785462e5b2042ce1955b3963c41c71c9b
allwells <aleenfestus@gmail.com>
2021-02-02 01:23:19 +0100

    Add TypeCast

---
+++ TypeCast/TypeCast.java
@@ -0,0 +1,27 @@
+public class TypeCast {
+    public static void main(String[] args) {
+    /**
+     * There are two types of casting
+     * - Widening Casting
+     * - Narrowing Casting
+     */
+        
+        byte byteNumb = 100;
+        System.out.println(byteNumb);
+
+        short shortNumb = (short) byteNumb;
+        System.out.println(shortNumb);
+
+        int intNumb = (int) shortNumb;
+        System.out.println(intNumb);
+
+        long longNumb = (long) intNumb;
+        System.out.println(longNumb);
+
+        float floatNumb = (float) longNumb;
+        System.out.println(floatNumb);
+
+        double doubleNumb = (double) floatNumb;
+        System.out.println(doubleNumb);
+    }
+}
