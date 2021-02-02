926dcd23b50ffb92065b6dc728562cf7c62c1e68
allwells <aleenfestus@gmail.com>
2021-02-02 09:22:16 +0100

    Add Subtraction in Operators

--- Operators/Operators.java
+++ Operators/Operators.java
@@ -10,13 +10,39 @@ public class Operators {
 
         /**
          * Java divides the operators into the following groups:
-         *  - Arithmetic Operators => { +, -, *, /, %, ++, -- }
-         *  - Assignment Operators => {  }
+         *  - Arithmetic Operators
+         *      [ '*' => Multiplication - Multiplies two values e.g. x * y ]
+         *      [ '/' => Division - Divides one values by another e.g. x / y]
+         *  - Assignment Operators
          *  - Comparison Operators
          *  - Logical Operators
          *  - Bitwise Operators
          * 
          */
+
+        /**
+         * ARITHMETICAL OPERATORS
+         * They are used to perform mathematical operations.
+         * They are grouped into 7:
+         * Addition(+), Subtraction(-), Multiplication(*), Division(/), Modulus(%), Increment(++), Decrement(--)
+         */
+
+         /**
+          * ADDITION(+)
+          * Adds together two values e.g. x + y
+          */
+          int x = 5;
+          int y = 2;
+          System.out.println("x + y = " + (x + y)); // Prints '7'
+
+          /**
+           * SUBTRACTION(-)
+           * Subtracts one values from another e.g. x - y
+           */
+          int a = 5;
+          int b = 2;
+          System.out.println("a - b = " + (a - b));
+
 
     }
 }
