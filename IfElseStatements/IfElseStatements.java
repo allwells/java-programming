51d9419c971729449dbb5e996d5097e8bd72f15d
allwells <aleenfestus@gmail.com>
2021-02-08 00:37:47 +0100

    Update Chapter: If...Else Statements =>  The if Statement

--- IfElseStatements/IfElseStatements.java
+++ IfElseStatements/IfElseStatements.java
@@ -1,5 +1,19 @@
 public class IfElseStatements {
     public static void main(String[] args) {
-        
+        /**
+         * IF...ELSE STATEMENTS
+         * if...else statements are conditional statements.
+         * Java has the following conditional statements:
+         * 'if' => to spto specify a block of code to be executed, if a specified condition is 'true'
+         * 'else' => to specify a block of code to be executed, if the same condition is 'false'
+         * 'else if' => to specify a new condition to test, if the first condition is 'false'
+         * 'switch' => to specify many alternative blocks of code to be executed
+         */
+
+         // The if Statement
+         // Syntax:
+         // if (condition) {
+         //    block of code to be executed if the condition is 'true'
+         // }
     }
 }
